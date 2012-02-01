5998848d2577353da8a7173677ba8009dbe8c972
chris <hodgsonc@rogers.com>
2012-02-01 09:20:22 -0500

    party validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ActiveRecord::Base
 
 validates :res_date, :res_time, :party, :patron_id, :last_name, :first_name, :supervisor, :presence => true
-
+validates_numericality_of :party, :only_integer => true, :message => "- Please Enter a Numberic Value"
 
 default_scope :order => "res_time" 
 
